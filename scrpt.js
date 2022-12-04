79bd3d6774e2b0f0b9d576e01f77ef7cb9381779
DimaDostoevskiy <dostoevskiy@list.ru>
2022-12-04 05:34:28 +0300

    fix/about

--- scrpt.js
+++ scrpt.js
@@ -1,18 +1,6 @@
-// setInterval(() => console.log(document.body.scrollHeight), 100);
-// for (const key in document) {
-//   if (document[key]) console.log(`${key} : ${document[key]}`);
-// }
-
-document.addEventListener("scroll", () => {
-    window.innerWidth = 500;
-    console.log(scrollY);
-})
-// const link = document.getElementsByClassName('navbar__link')
-const burgerButton = document.getElementById("navbar__button");
-const navbarMenu = document.getElementById('navbar__menu');
-const navbarLogo = document.getElementById('navbar__logo');
-
-let isActive = false;
+const burgerButton = document.getElementsByClassName("navbar__button")[0];
+const navbarMenu = document.getElementsByClassName("navbar__menu")[0];
+const navbarLogo = document.getElementsByClassName("navbar__logo")[0];
 
 burgerButton.addEventListener("click", () => {
   burgerButton.classList.toggle('navbar__button_active');
@@ -20,19 +8,6 @@ burgerButton.addEventListener("click", () => {
   navbarLogo.classList.toggle('navbar__logo_active');
 });
 
-
-// navbarLogo.addEventListener("click", () => {
-//   burgerButton.classList.toggle('navbar__button_active');
-//   navbarMenu.classList.toggle('navbar__menu_active');
-//   navbarLogo.classList.toggle('navbar__logo_active');
-//   console.log(`hi`);
-// });
-// burgerButton.addEventListener("click", () => {
-//   console.log(burgerButton);
-// });
-
-// link[0].addEventListener('click', ()=>{
-//     console.log(link[0].style);
+// document.addEventListener("scroll", () => {
+//     console.log(scrollY);
 // })
-
-// console.log();
